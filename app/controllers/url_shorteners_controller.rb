7daa6fecf1c5be32c30f09ce7eaa48d42cb300c5
roynyaga <nyagaandreroy@gmail.com>
2021-04-26 17:50:32 +0100

    add create action and url_shortener_params

--- app/controllers/url_shorteners_controller.rb
+++ app/controllers/url_shorteners_controller.rb
@@ -1,4 +1,23 @@
 class UrlShortenersController < ApplicationController
   def index
+    @url = UrlShortener.new
+  end
+  
+  def create 
+    @url = UrlShortener.new(url_shorteners)
+    if @url.save 
+      @url.save
+      flash.now[:success] = "successfully created url short form"
+      render "index"
+    else
+      flash.now[:warning] = "could not create shortenner for the following reasons"
+      render "index"
+    end 
+  end
+  
+  private 
+
+  def url_shortener_params 
+    params.require(:url_shortner).permit(:original, :short_version)
   end 
 end
