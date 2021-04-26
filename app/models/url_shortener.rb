cb7d317dac4607405ed5e72a3a5b3925e77a3dd8
roynyaga <nyagaandreroy@gmail.com>
2021-04-26 19:01:20 +0100

    create add_shortform and extract_protocol_and_domain

--- app/models/url_shortener.rb
+++ app/models/url_shortener.rb
@@ -1,2 +1,16 @@
 class UrlShortener < ApplicationRecord
+
+  def add_shortform 
+    short_version = "#{extract_protocol_and_domain}#{SecureRandom.hex(2)}"
+  end 
+
+  private 
+
+  def extract_protocol_and_domain 
+    fore_slash_count = 0
+    origin.each_char.with_index(1) do |char, i|
+      fore_slash_count += 1 if char == "/"
+      return original.take(i) if fore_slash_count == 3
+    end 
+  end 
 end
